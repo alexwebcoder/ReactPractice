c085034bbf6f55cd8ab8eb8ea460f0cd23c32807
alexwebcoder <alexdeleon@stayup.com>
2023-07-02 16:01:27 -0700

    added conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ const App = () => {
     return (
      
         <Card 
+          key={item.id}
           img={item.coverImg}
           rating={item.stats.rating}
           reviewCount={item.stats.reviewCount}
           location={item.location}
           title={item.title}
           price={item.price}
+          openSpots={item.openSpots}
         />
     )
   })
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,7 +1,17 @@
 
-const Card = ({img, rating, reviewCount, location, title, price}) => {
+const Card = ({img, rating, reviewCount, location, title, price, key, openSpots}) => {
+    console.log({openSpots, key});
+     
+    let  badgeText;
+    if (openSpots === 0) {
+        badgeText = 'SOLD OUT'
+    } else if (location === 'Online') {
+        badgeText = 'ONLINE'
+    }
+
     return (
         <div className="card">
+            {badgeText && <div className="card-badge">{badgeText}</div>}
             <figure><img className="card-image" src={`../images/${img}`} alt="image of whoever"/></figure>
             <figure><img className="card-star" src="./star.png" alt="image of star" width="20px"/><figcaption className="gray">{rating} ({reviewCount})- {location}</figcaption></figure>
             <p className="card-title">{title}</p>
