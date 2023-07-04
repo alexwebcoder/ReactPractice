f645140ffa074f87fc73d8f99f352ad90b9407db
alexwebcoder <alexdeleon@stayup.com>
2023-07-04 08:36:02 -0700

    added image generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,13 +1,24 @@
+import memesData from "../memesData";
+
 const Meme = () => {
+ 
+    const getRandomImage = () => {
+        const imageArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * imageArray.length);
+        const url = imageArray[randomNumber].url;
+        console.log(url);
+    }
+
+    
     return (
         <main>
-            <form className="form" action="">
+            <div className="form" action="">
                 <div>
                     <input type="text" placeholder="top text"/>
                     <input type="text"  placeholder="bottom text" />
                 </div>
-            <button className="button" type="submit">Get a new meme image</button>
-            </form>
+            <button className="button" type="submit" onClick={getRandomImage}>Get a new meme image</button>
+            </div>
         </main>
     )
 }
